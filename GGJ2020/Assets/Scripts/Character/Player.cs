03b182c2a1ecf57693f489cc1ea98641264b6cc6
Benny <benny.zoeller@gmail.com>
2020-01-31 23:30:27 +0100

    Added InputSystem

---
+++ GGJ2020/Assets/Scripts/Character/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : Character
+{
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Move(Vector2.up);
+        }
+    }
+}
--- GGJ2020/Assets/Scripts/Interactables/Interactable.cs
+++ GGJ2020/Assets/Scripts/Interactables/Interactable.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Interact()
     {
-        
+
     }
 }
