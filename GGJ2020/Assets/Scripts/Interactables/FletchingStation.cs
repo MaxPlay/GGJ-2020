57cef3d46ca762f609f38606779b4870a0bb48f7
Benny <benny.zoeller@gmail.com>
2020-01-31 23:02:02 +0100

    Creatied first basic Classes

--- GGJ2020/Assets/Scripts/Interactables/FletchingStation.cs
+++ GGJ2020/Assets/Scripts/Interactables/FletchingStation.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour
+public class FletchingStation : Interactable
 {
-
-
     // Start is called before the first frame update
     void Start()
     {
--- GGJ2020/Assets/Scripts/Interactables/Interactable.cs
+++ GGJ2020/Assets/Scripts/Interactables/Interactable.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour
+public class Interactable : MonoBehaviour
 {
-
-
     // Start is called before the first frame update
     void Start()
     {
