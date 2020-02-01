f64f72ef4c8ea406c9576bfc7d0d6008535252d3
MaxPlay <maxplayyt@gmail.com>
2020-02-01 23:22:46 +0100

    Made the furnace light up and flicker Fixed some minor things in the other station prefabs

---
+++ GGJ2020/Assets/Scripts/Rendering/LightFlicker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class LightFlicker : MonoBehaviour
+{
+    [SerializeField]
+    AnimationCurve curve;
+
+    float time;
+
+    new Light light;
+
+    float baseIntensity;
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+        baseIntensity = light.intensity;
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 1)
+            time -= 1;
+
+        light.intensity = baseIntensity + curve.Evaluate(time);
+    }
+}
