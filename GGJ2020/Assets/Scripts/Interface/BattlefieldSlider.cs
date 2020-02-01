212f855ca6284ef5ca5395389f3a6121339e509e
nico <sanesaidpolly@gmail.com>
2020-02-01 17:37:46 +0100

    Added Battlefield Slider Prefab for Score

---
+++ GGJ2020/Assets/Scripts/Interface/BattlefieldSlider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattlefieldSlider : MonoBehaviour {
+
+    [SerializeField]
+    int initialScore = 10;
+    [SerializeField]
+    int scoreWin = 50;
+    [SerializeField]
+    int scoreLose = 0;
+
+    private Slider slider;
+
+    // Start is called before the first frame update
+    void Start () {
+
+        slider = GetComponent<Slider> ();
+
+        // SetupScoreRange (scoreLose, scoreWin, initialScore);
+        // SetScore (initialScore);
+
+    }
+
+    public void SetupScoreRange (int min, int max, int start) {
+
+        slider.minValue = min;
+
+        slider.maxValue = max;
+
+        slider.value = start;
+    }
+
+    public void SetScore (int score) {
+
+        slider.value = score;
+
+    }
+
+}
