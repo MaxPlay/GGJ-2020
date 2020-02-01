d1c7465c961a4f614ac0b2ca03030c5c62ef780d
nico <sanesaidpolly@gmail.com>
2020-02-01 10:41:39 +0100

    Added StationInteractable

---
+++ GGJ2020/Assets/Scripts/Interactables/StationInteractable.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationInteractable : MonoBehaviour
+{
+
+
+//von Benny
+[SerializeField]
+private bool stationIsInUse;
+[SerializeField]
+private float itemGrindingValue;
+[SerializeField]
+private float itemGrindingStartValue;
+[SerializeField]
+private float itemGrindingFinishValue;
+
+
+//inner logic
+
+[SerializeField]
+private float progressSpeed;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+ void OnTriggerStay(Collider other)
+    {
+        if ((other.gameObject.GetComponent<Player>() != null) && (stationIsInUse))
+        {
+
+
+
+
+
+
+
+
+        
+        }
+
+        else if ((other.gameObject.GetComponent<Player>() != null) && (!stationIsInUse))
+
+        {
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+private void UseStation(int stationIdentifier) {
+
+
+
+
+
+
+switch(stationIdentifier) {
+
+case 0:
+//Grinding
+
+//Todo Animation
+
+itemGrindingValue += progressSpeed * Time.deltaTime;
+
+float percentDone = GetProgressInPercent(itemGrindingStartValue, itemGrindingFinishValue, itemGrindingValue);
+
+//Give to Progressbar
+
+
+break;
+
+
+
+
+default:
+
+Debug.Log("UseStation: The fuck happened?")
+
+break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+}
+
+
+
+private float GetProgressInPercent(float start, float finish, float present) {
+
+float hundredPercent = finish - start;
+
+float percentage = (present - start) / hundredPercent * 100;
+
+return percentage;
+
+}
+
+
+
+}
