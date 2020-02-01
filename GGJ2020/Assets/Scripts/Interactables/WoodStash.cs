219562d0045764a5e74bc7f457a822cd33285449
Benny <benny.zoeller@gmail.com>
2020-02-01 17:43:38 +0100

    Created Woodstash & Prefabs

---
+++ GGJ2020/Assets/Scripts/Interactables/WoodStash.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodStash : Interactable
+{
+    [SerializeField]
+    private GameObject woodPrefab;
+
+    public override Interactable Interact(Character character)
+    {
+        if(character.Inventory == null)
+        {
+            Wood spawnedWood = Instantiate(woodPrefab).GetComponent<Wood>();
+
+            character.PickUpItem(spawnedWood);
+        }
+
+        return this;
+    }
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+    }
+}
