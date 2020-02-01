5487b4c5ad5f0fbde6871ecba36ac4d3a3af41cd
Benny <benny.zoeller@gmail.com>
2020-02-01 10:50:53 +0100

    Added Item Sharpening

--- GGJ2020/Assets/Scripts/Items/Sword.cs
+++ GGJ2020/Assets/Scripts/Items/Sword.cs
@@ -4,8 +4,44 @@ using UnityEngine;
 
 public class Sword : Item
 {
+    private bool hasHandle;
+    private float sharpness;
+    private float quality;
+    private float heat;
+
+    public bool HasHandle { get => hasHandle; }
+    public float Sharpness { get => sharpness; }
+    public float Quality { get => quality; }
+    public float Heat { get => heat; }
+
     public override Interactable Interact(Character character)
     {
         return base.Interact(character);
+    }
+
+    public float HeatSword(float strength)
+    {
+        heat += strength;
+        return heat;
+    }
+
+    public float SharpenSword(float strength)
+    {
+        sharpness = Mathf.Clamp01(sharpness + strength);
+        return sharpness;
+    }
+
+    public float HammerSword(float strength)
+    {
+        quality = Mathf.Clamp01(quality + strength);
+        return quality;
+    }
+
+    public bool AddHandle()
+    {
+        if (hasHandle)
+            return false;
+        hasHandle = true;
+        return true;
     }
 }
