d00b65a1307ed8fbe165a7c4420423952394727e
nico <sanesaidpolly@gmail.com>
2020-02-01 23:18:11 +0100

    Added Game-Over-Screen

--- GGJ2020/Assets/Scripts/Interface/BattlefieldSlider.cs
+++ GGJ2020/Assets/Scripts/Interface/BattlefieldSlider.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class BattlefieldSlider : MonoBehaviour {
 
+private PanelManager panelManager;
+
     private Slider slider;
 
     [SerializeField]
@@ -38,27 +40,37 @@ public class BattlefieldSlider : MonoBehaviour {
 
     private int scoreWon;
 
+    private bool gameIsOver = false;
+
     void Start () {
+
+panelManager = GameObject.Find("PanelNotifications").GetComponent<PanelManager>();
 
         slider = GetComponent<Slider> ();
 
         SetupScoreRange ();
 
+
+
+        //TEST
+
+        InitAndStartBattlePointLoop();
+
     }
 
     public void SetupScoreRange () {
 
-        slider.minValue = GameManager.Instance.GetComponent<GameplaySettings> ().ScoreLose;
+        slider.minValue = GameManager.Instance.Settings.ScoreLose;
 
-        slider.maxValue = GameManager.Instance.GetComponent<GameplaySettings> ().ScoreWin;
+        slider.maxValue = GameManager.Instance.Settings.ScoreWin;
 
-        slider.value = GameManager.Instance.GetComponent<GameplaySettings> ().Score;
+        slider.value = GameManager.Instance.Settings.Score;
 
-        gameScore = GameManager.Instance.GetComponent<GameplaySettings> ().Score;
+        gameScore = GameManager.Instance.Settings.Score;
 
-        scoreWon = GameManager.Instance.GetComponent<GameplaySettings> ().ScoreWin;
+        scoreWon = GameManager.Instance.Settings.ScoreWin;
 
-        scoreLost = GameManager.Instance.GetComponent<GameplaySettings> ().ScoreLose;
+        scoreLost = GameManager.Instance.Settings.ScoreLose;
 
     }
 
@@ -74,9 +86,23 @@ public class BattlefieldSlider : MonoBehaviour {
     //**********************************************************************************************
     //**********************************************************************************************
 
-    public void LooseGame () { Debug.Log ("DUMMY: YOU LOST YOU IDIOT!"); }
+    public void LooseGame () { 
+        
+        Debug.Log ("DUMMY: YOU LOST YOU IDIOT!");
+        
+        gameIsOver = true;
 
-    public void WinGame () { Debug.Log ("DUMMY: YOU WON YOU ARROGANT PRICK!"); }
+        panelManager.ShowNotification("YOU LOST KAREN!");
+         }
+
+    public void WinGame () {
+        
+         Debug.Log ("DUMMY: YOU WON YOU ARROGANT PRICK!");
+
+        panelManager.ShowNotification("YOU WON PIKACHU!");
+         
+           gameIsOver = true;
+          }
 
     public int CountCustomers () {
 
@@ -93,22 +119,24 @@ public class BattlefieldSlider : MonoBehaviour {
 
     public void InitAndStartBattlePointLoop () {
 
-        secondsPerRound = GameManager.Instance.GetComponent<GameplaySettings> ().SecondsPerRound;
+        secondsPerRound = GameManager.Instance.Settings.SecondsPerRound;
 
-        customersWithoutPenalty = GameManager.Instance.GetComponent<GameplaySettings> ().CustomersWithoutPenalty;
+        customersWithoutPenalty = GameManager.Instance.Settings.CustomersWithoutPenalty;
 
-        penaltyEndOfRoundPerCustomer = GameManager.Instance.GetComponent<GameplaySettings> ().PenaltyEndOfRoundPerCustomer;
+        penaltyEndOfRoundPerCustomer = GameManager.Instance.Settings.PenaltyEndOfRoundPerCustomer;
 
-        ratioPointsCustomerSatisfied = GameManager.Instance.GetComponent<GameplaySettings> ().RatioPointsCustomerSatisfied;
+        ratioPointsCustomerSatisfied = GameManager.Instance.Settings.RatioPointsCustomerSatisfied;
 
-        ratioPointsCustomerImpressed = GameManager.Instance.GetComponent<GameplaySettings> ().RatioPointsCustomerImpressed;
+        ratioPointsCustomerImpressed = GameManager.Instance.Settings.RatioPointsCustomerImpressed;
 
-        ratioPointsCustomerDiappointed = GameManager.Instance.GetComponent<GameplaySettings> ().RatioPointsCustomerDiappointed;
+        ratioPointsCustomerDiappointed = GameManager.Instance.Settings.RatioPointsCustomerDiappointed;
 
         StartCoroutine (LoosePointsOverTime ());
     }
 
     IEnumerator LoosePointsOverTime () {
+
+        while(!gameIsOver) {
 
         int waitingCustomers = CountCustomers ();
 
@@ -118,11 +146,18 @@ public class BattlefieldSlider : MonoBehaviour {
 
             gameScore -= penalty;
 
+            SetScore(gameScore);
+
+            Debug.Log ("LoosePointsOverTime: Your penalty is: "+penalty+", waiting are: "+waitingCustomers);
+
+
             if (gameScore <= scoreLost) { LooseGame (); }
 
-        }
+        } else {Debug.Log ("LoosePointsOverTime: No penalty for you!");}
 
         yield return new WaitForSeconds (secondsPerRound);
+
+        }
 
     }
 
@@ -131,5 +166,8 @@ public class BattlefieldSlider : MonoBehaviour {
         if (gameScore >= scoreWon) { WinGame (); }
 
     }
+
+
+
 
 }
