4243d43bf73e1f165f08239c44946a2c25a9f770
MaxPlay <maxplayyt@gmail.com>
2020-02-02 10:18:02 +0100

    Created classes for storing controls

---
+++ GGJ2020/Assets/Scripts/Data/ControlSettings.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Control Settings")]
+public class ControlSettings : ScriptableObject
+{
+    [SerializeField]
+    private KeyCode up;
+
+    public KeyCode Up
+    {
+        get { return up; }
+        set { up = value; }
+    }
+
+    [SerializeField]
+    private KeyCode down;
+
+    public KeyCode Down
+    {
+        get { return down; }
+        set { down = value; }
+    }
+
+    [SerializeField]
+    private KeyCode left;
+
+    public KeyCode Left
+    {
+        get { return left; }
+        set { left = value; }
+    }
+
+    [SerializeField]
+    private KeyCode right;
+
+    public KeyCode Right
+    {
+        get { return right; }
+        set { right = value; }
+    }
+
+    [SerializeField]
+    private KeyCode interact;
+
+    public KeyCode Interact
+    {
+        get { return interact; }
+        set { interact = value; }
+    }
+
+    [SerializeField]
+    private KeyCode pause;
+
+    public KeyCode Pause
+    {
+        get { return pause; }
+        set { pause = value; }
+    }
+}
---
+++ GGJ2020/Assets/Scripts/Data/Controls.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+[Serializable]
+public class Controls
+{
+    [SerializeField]
+    ControlSettings inputTemplate;
+
+    [SerializeField]
+    ControlSettings alternativeInputTemplate;
+
+    ControlSettings input;
+    ControlSettings alternativeInput;
+
+    public string InputFile => Path.Combine(Application.persistentDataPath, "input.duck");
+    public string AlternativeInputFile => Path.Combine(Application.persistentDataPath, "alt_input.duck");
+
+    public ControlSettings Input => input;
+    public ControlSettings AlternativeInput => alternativeInput;
+
+    public void Load()
+    {
+        LoadData(InputFile, ref input, inputTemplate);
+        LoadData(AlternativeInputFile, ref alternativeInput, alternativeInputTemplate);
+    }
+
+    private void LoadData(string inputFile, ref ControlSettings input, ControlSettings template)
+    {
+        if (File.Exists(inputFile))
+        {
+            input = JsonUtility.FromJson<ControlSettings>(File.ReadAllText(inputFile));
+        }
+        else
+        {
+            input = ScriptableObject.CreateInstance<ControlSettings>();
+            input.Down = template.Down;
+            input.Right = template.Right;
+            input.Up = template.Up;
+            input.Left = template.Left;
+            input.Interact = template.Interact;
+            input.Pause = template.Pause;
+            File.WriteAllText(inputFile, JsonUtility.ToJson(input));
+        }
+    }
+
+    public void Save()
+    {
+        if (File.Exists(InputFile))
+            File.Delete(InputFile);
+        if (File.Exists(AlternativeInputFile))
+            File.Delete(AlternativeInputFile);
+
+        File.WriteAllText(InputFile, JsonUtility.ToJson(input));
+        File.WriteAllText(AlternativeInputFile, JsonUtility.ToJson(alternativeInput));
+    }
+}
