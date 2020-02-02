557dd62699e4ae2a2fb2c370d08f44f6b34939ef
nico <sanesaidpolly@gmail.com>
2020-02-02 14:16:24 +0100

    Added sound files

---
+++ GGJ2020/Assets/Scripts/Interface/PlayerWalkAndGrunt.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalkAndGrunt : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip sound;
+
+bool soundOn = true;
+
+    void Start()
+    {
+          audioSource = GetComponent<AudioSource> ();
+
+
+   StartCoroutine (PlayerHasMoved ());
+
+    }
+
+
+
+ IEnumerator PlayerHasMoved () {
+
+        while (soundOn) {
+
+Vector3 earlier = transform.position;
+
+float bla = 0.1f;
+
+yield return new WaitForSeconds (bla);
+
+Vector3 later = transform.position;
+
+if (earlier != later) {
+
+     audioSource.clip = sound;
+            audioSource.Play ();
+
+
+
+} 
+
+
+
+           
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+}
